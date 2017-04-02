bc1c4ff61b0155ccb39e3dd73d1719d86abb98c1
zmn <zemmapan@altervista.org>
2017-04-02 19:10:29 +0200

    added project for nuget reference tests

--- HubLink.NugetTests/Program.cs
+++ HubLink.NugetTests/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+using log4net;
 
 namespace HubLink.NugetTests
 {
@@ -10,6 +12,16 @@ namespace HubLink.NugetTests
     {
         static void Main(string[] args)
         {
+
+            Logger nlogLogger = NLog.LogManager.GetLogger("wlog.logger");
+            ILog log4netLogger = log4net.LogManager.GetLogger(typeof(Program));
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                nlogLogger.Info("LOG NLOG #" + i);
+                log4netLogger.Info("LOG LOG4NET #" + i);
+            }
         }
     }
 }
